bda94b26ef5cf44912879263ffdd1f15f8bcc6e9
dedKRV <arsenij022018@gmail.com>
2025-09-12 20:30:01 +0300

    LED WHITE.py

--- LED WHITE.py
+++ LED WHITE.py
@@ -1,4 +1,4 @@
-import sys
+import sys #https://github.com/dedKRV/Color-Music-on-Python-and-Arduino-WS2812B-ESP32
 import json
 import numpy as np
 import sounddevice as sd
@@ -285,4 +285,5 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = LEDControl()
     window.show()
+
     sys.exit(app.exec_())
